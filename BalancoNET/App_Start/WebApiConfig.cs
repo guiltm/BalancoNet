416029227e8680a8ad841a084dbe19f3003b07d9
Thiago Montenegro <thiago.montenegro@softlognet.com.br>
2015-12-29 02:15:40 -0300

    #Tarefa S/N - Adicionando projeto ao repositorio.

---
+++ BalancoNET/App_Start/WebApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BalancoNET
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+
+            // adicionando newtonsoft como o serializador padrão
+
+            // referencia o objeto duplicado
+            jsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter); // Removendo o xml, garantir que vou passar apenas json
+            config.EnableCors(); //Install-Package Microsoft.AspNet.WebApi.Cors
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
